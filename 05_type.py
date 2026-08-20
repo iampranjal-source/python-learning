59cc463e6f598f880d734e6d9108f3b750813777
Pranjal Chauhan <pranjalchauhan304@gmail.com>
2026-08-20 14:16:53 +0530

    initail commit

---
+++ 05_type.py
@@ -0,0 +1,24 @@
+a =  31
+t = type(a) # 'a' is class <int>
+print(a)
+print(t)
+
+a = 67.67672
+t = type(a) #'a' is class <float number>
+print(t)
+
+a = "joseph"
+t = type(a) # 'a' is class <string>
+print(t)
+
+g = True
+h = type(g) # "g" is class <boolean>. hi 
+print(h)
+
+a = "31.2"
+b = float(a) # b should be a but the type be float 
+g = type(b)
+t = type(a)
+print(t)
+print(b)
+print(g)
