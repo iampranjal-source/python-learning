23228f64a00128bdee183aa756dd9068996a4834
iampranjal-source <pranjalchauhan304@gmail.com>
2026-08-23 22:49:11 +0530

    Add files via upload

---
+++ 01_intro_to_strings.py
@@ -0,0 +1,12 @@
+name = "harry"
+# from left to right start with 0 then 1 etc.
+# from ight to left start with -1 then -2 etc.
+
+nameshort = name[0:3] #start from zero amd goes all the way to 3(excluding 3)
+print(nameshort)
+
+character1 = name[4]
+print(character1)
+
+character2 = name [-5]
+print(character2)
