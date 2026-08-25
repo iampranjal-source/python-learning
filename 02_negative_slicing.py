527311326667d866dbe7817bedeb90de5e064abe
iampranjal-source <pranjalchauhan304@gmail.com>
2026-08-25 22:06:17 +0530

    Add files via upload

---
+++ 02_negative_slicing.py
@@ -0,0 +1,11 @@
+name = "Harryyiod"
+
+nameshort = name[-5:-1]
+nameshort1 = name[4:8]
+print(nameshort)
+
+print(nameshort1)
+
+print(name[:4]) #same as print(name[0:4])
+print(name[1:]) #same as print(name[1:8])
+print(name[1:8])
---
+++ 03_str_functions.py
@@ -0,0 +1,11 @@
+name = "harry is good boy"
+
+print(len(name))
+print(name.endswith("ry"))
+print(name.startswith("harry"))
+print(name.capitalize())
+print(name.title())
+print(name.upper())
+
+# strings are not mutable, here the changes do not edit the strings but create new string with the changes. 
+# hence string cannot be edited and here new string is created with desired changes.
