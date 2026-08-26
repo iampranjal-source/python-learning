99e7f8cb61fe8ce2b5f4dc1631a11f79dd11f2a9
iampranjal-source <pranjalchauhan304@gmail.com>
2026-08-26 11:57:19 +0530

    Add files via upload

---
+++ 01_problem_1.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+
+print(f"TMKOC {name} ")
+
+name = input("Enter your name: ")
+
+print(f"Good Morning {name} ")
---
+++ 04_escape_sequence.py
@@ -0,0 +1,11 @@
+a = "Harry is a good \nboy but not \na bad \"boy\""
+print(a)
+
+a = "Harry is a good \tboy but not \ta bad \"boy\""
+print(a)
+
+a = "Harry\'s a good \tboy but not \ta bad \"boy\""
+print(a)
+
+a = "Harry \\is a good \tboy but not \ta bad \"boy\""
+print(a)     
