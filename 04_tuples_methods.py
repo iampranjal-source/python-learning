6791641313b7c46d04fb789f1d2dfcbe91d2bcee
iampranjal-source <pranjalchauhan304@gmail.com>
2026-08-30 11:23:20 +0530

    Add files via upload

---
+++ 04_tuples_methods.py
@@ -0,0 +1,11 @@
+a = ( 1, 45, 856, 85471, "Rohan", False)
+print(a)
+
+no = a.count(45)
+print(no)
+
+a = ( 1, 45, 856, 85471, "Rohan", 45, False)
+print(a)
+
+no = a.count(45) # count tells ki 45 us tuple kitni baar aata hai
+print(no)
