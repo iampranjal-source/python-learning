1c31ca0bf0dd11009f7cb51ef1e6e127fcfef49a
iampranjal-source <pranjalchauhan304@gmail.com>
2026-08-31 22:48:01 +0530

    Add files via upload

---
+++ 02_problems2.py
@@ -0,0 +1,45 @@
+marks = []
+f1 = input("Enter marks here: ")
+marks.append(f1)
+f2 = input("Enter marks here: ")
+marks.append(f2)
+f3 = input("Enter marks here: ")
+marks.append(f3)
+f4 = input("Enter marks here: ")
+marks.append(f4)
+f5 = input("Enter marks here: ")
+marks.append(f5)
+f6 = input("Enter marks here: ")
+marks.append(f6)
+f7 = input("Enter marks here: ")
+marks.append(f7)
+
+marks.sort()
+
+print(marks)
+
+#strings are compared character by character, not by their numerical value.strings, not numbers.
+#Python compares the characters from left to right.
+
+marks = []
+f1 = int(input("Enter marks here: "))
+marks.append(f1)
+f2 = int(input("Enter marks here: "))
+marks.append(f2)
+f3 = int(input("Enter marks here: "))
+marks.append(f3)
+f4 = int(input("Enter marks here: "))
+marks.append(f4)
+f5 = int(input("Enter marks here: "))
+marks.append(f5)
+f6 = int(input("Enter marks here: "))
+marks.append(f6)
+f7 = int(input("Enter marks here: "))
+marks.append(f7)
+
+
+marks.sort()
+
+print(marks)
+
+#after applying the integer function all the marks are arranged in sorted form
