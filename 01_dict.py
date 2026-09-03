98ecfc01915c24653d2239e3499fbd6bf40e35e8
iampranjal-source <pranjalchauhan304@gmail.com>
2026-09-03 21:40:08 +0530

    Add files via upload

---
+++ 01_dict.py
@@ -0,0 +1,18 @@
+#marks = {"Harry": 100,
+#         "Shubham": 56,
+#         "Rohan": 45}
+#
+#print(marks, type(marks))
+#print(marks("Harry"))
+
+# marks["Harry"]     # [] → access dictionary value
+# print("Hello")     # () → call a function, when we use () python thinks that we are calling out to a function 
+# and when we [] it commands python to access the dict and carry the function.
+
+
+marks = {"Harry": 100,
+         "Shubham": 56,
+         "Rohan": 45}
+
+print(marks, type(marks))
+print(marks["Harry"])
