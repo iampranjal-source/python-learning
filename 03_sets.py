345a02348f59bcc263fefee150db386615511b18
iampranjal-source <pranjalchauhan304@gmail.com>
2026-09-08 18:48:44 +0530

    Add files via upload

---
+++ 03_sets.py
@@ -0,0 +1,5 @@
+e = set() #dont use s = {} as it will create an empty dict.
+
+s = {1, 5, 56, 7, 7, 7, 7}
+
+print(s) # When Python creates a set, it stores the elements using a structure called a hash table. The position of an element is determined by its hash value, rather than by the order in which you typed it.
