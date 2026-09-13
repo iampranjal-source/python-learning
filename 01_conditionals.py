fabc93f1fac40043dca84cb76543c9fd581ae868
iampranjal-source <pranjalchauhan304@gmail.com>
2026-09-13 23:52:32 +0530

    Add files via upload

---
+++ 01_conditionals.py
@@ -0,0 +1,12 @@
+# if else statement.
+a = int(input("Enter you age : "))
+
+if(a>=18):
+    print("You are above the age of consent")
+    print("You are good")
+
+else:
+    print("You are below the age limit")
+
+
+print("End of the programme")
