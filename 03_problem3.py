c617ce2d07b31919b449206663a9d3508da3b640
iampranjal-source <pranjalchauhan304@gmail.com>
2026-09-17 19:38:30 +0530

    Add files via upload

--- 03_problem3.py
+++ 03_problem3.py
@@ -1,7 +1,35 @@
-a = (34, 563, "Harry")
-a[2] = 45
+#spam_comment1 = input("Enter spam comment : ")
 
-a = (34, 563, "Harry")
-a[2] = "larry"
+#if spam_comment1 in ["Make a lot of money", "buy now", "subscribe this", "click this"]:
+  #  print("This is a spam comment")
 
-#tupple type cannot be changed in python
+#if spam_comment2 in ["Make a lot of money", "buy now", "subscribe this", "click this"]:
+   # print("This is a spam comment")
+
+#spam_comment3 = input("Enter spam comment : ")
+#if spam_comment3 in ["Make a lot of money", "buy now", "subscribe this", "click this"]:
+   # print("This is a spam comment")
+
+#spam_comment4 = input("Enter spam comment : ")
+#if spam_comment4 in ["Make a lot of money", "buy now", "subscribe this", "click this"]:
+ #   print("This is a spam comment")
+
+
+
+
+p1 = "Make a lot of money"
+p2 = "buy now"
+p3 = "subscribe this"
+p4 = "click this"
+
+message = input("Enter your comment : ")
+
+if((p1 in message) or (p2 in message) or (p3 in message) or (p4 in message)):
+    print("This is a spam comment")
+
+else:
+    print("This is not a spam comment")
+
+
+
+    
